28f016b0795d30aece7a258466383c61a5ee7527
sofierk <sofiekarasek@berkeley.edu>
2015-04-29 22:51:48 -0700

    home

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+$(".hidden").hide();
+$("#infographic").delay(4000).fadeOut(5000).removeClass(".infographic1");
+$(".hidden").delay(4000).fadeIn(5000).addClass(".infographic2");
+
+});
