b2b10d6174f5eb6487095b39973b9c5534859c61
sofierk <sofiekarasek@berkeley.edu>
2015-04-29 23:33:25 -0700

    changing delay time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $(".hidden").hide();
-$("#infographic").delay(4000).fadeOut(5000).removeClass(".infographic1");
-$(".hidden").delay(4000).fadeIn(5000).addClass(".infographic2");
+$("#infographic").delay(3000).fadeOut(4000).removeClass(".infographic1");
+$(".hidden").delay(3000).fadeIn(4000).addClass(".infographic2");
 
 });
